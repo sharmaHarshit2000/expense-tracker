08a35f014abede1c56550d8400b611f9845514f5
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-20 15:36:22 +0530

    setup basic server

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
 
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
