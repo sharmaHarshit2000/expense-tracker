1800b7ff07180df1254c0a6e6cae9f0774368260
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-20 16:48:14 +0530

    NotFound and Global Error handler setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ import connectDB from "./config/db.js";
 //Routes
 import authRoutes from "./routes/authRoutes.js"
 
+// Not Found and Global Error handler
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
 // Config
 dotenv.config();
 connectDB();
@@ -19,6 +23,10 @@ app.get("/", (req, res) => {
 
 // Route mounting
 app.use("/api/auth", authRoutes)
+
+//Error handler
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
