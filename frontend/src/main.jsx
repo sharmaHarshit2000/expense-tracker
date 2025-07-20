ab8097895f82516d28877e353c8175df01962abf
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-21 00:00:48 +0530

    react toast added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
       <StrictMode>
         <App />
+        <Toaster position="top-center" />
       </StrictMode>
     </AuthProvider>
   </BrowserRouter>
