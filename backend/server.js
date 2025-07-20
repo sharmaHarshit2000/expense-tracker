69b7c8fe95001688468317b05695a665219b9ee6
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-20 16:38:07 +0530

    setup the User model, controller and routes

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+//Routes
+import authRoutes from "./routes/authRoutes.js"
+
+// Config
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
 
+// Testing API
+app.get("/", (req, res) => {
+  res.send("expense-tracker-backend API is running...");
+});
+
+// Route mounting
+app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
