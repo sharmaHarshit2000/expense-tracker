9dddafca22a1e6f1fc0d6e08cc8f50d2698ade87
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-21 13:46:07 +0530

    add a loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[200px]">
+      <div className="w-12 h-12 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
