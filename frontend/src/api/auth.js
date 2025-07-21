9887267c06511c7640d8b8cc4b70df1fe1b72fd3
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-21 13:50:28 +0530

    add me route

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,3 +2,4 @@ import api from "./axios";
 
 export const loginUser = (data) => api.post("/auth/login", data);
 export const registerUser = (data) => api.post("/auth/register", data);
+export const getMe = () => api.get("/auth/me");
