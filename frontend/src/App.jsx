859ccb5c60c241a3012efd7bad2043c04e04460b
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-22 10:58:19 +0530

    Lazy loading implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,65 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import ExpenseForm from "./pages/ExpenseForm";
-import AdminPanel from "./pages/AdminPanel";
-import AuditLogs from "./pages/AuditLogs";
-import RegisterPage from "./pages/RegisterPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Loader from "./components/Loader";
 import { Box } from "@mui/material";
-import NotFound from "./pages/NotFound";
+
+// Lazy loaded pages
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const ExpenseForm = lazy(() => import("./pages/ExpenseForm"));
+const AdminPanel = lazy(() => import("./pages/AdminPanel"));
+const AuditLogs = lazy(() => import("./pages/AuditLogs"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
 const App = () => {
     return (
         <Box className="flex flex-col min-h-screen">
             <Header />
 
             <Box component="main" className="flex-grow">
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route
-                        path="/dashboard"
-                        element={
-                            <ProtectedRoute>
-                                <Dashboard />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/add-expense"
-                        element={
-                            <ProtectedRoute>
-                                <ExpenseForm />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/admin"
-                        element={
-                            <ProtectedRoute adminOnly>
-                                <AdminPanel />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/audit-logs"
-                        element={
-                            <ProtectedRoute adminOnly>
-                                <AuditLogs />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route path="*" element={<NotFound />} /> 
-                </Routes>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route
+                            path="/dashboard"
+                            element={
+                                <ProtectedRoute>
+                                    <Dashboard />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/add-expense"
+                            element={
+                                <ProtectedRoute>
+                                    <ExpenseForm />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/admin"
+                            element={
+                                <ProtectedRoute adminOnly>
+                                    <AdminPanel />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/audit-logs"
+                            element={
+                                <ProtectedRoute adminOnly>
+                                    <AuditLogs />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Suspense>
             </Box>
 
             <Footer />
